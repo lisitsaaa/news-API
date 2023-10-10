865b698992184b7c188bddf637ba5ad2de0f9e14
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-10-11 00:44:00 +0300

    Added UserMapper

---
+++ src/main/java/com/example/newsapi/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.example.newsapi.mapper;
+
+import com.example.newsapi.dto.user.RegDto;
+import com.example.newsapi.dto.user.UserDto;
+import com.example.newsapi.entity.user.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User regDtoToUser(RegDto dto);
+    UserDto userToUserDto(User user);
+}
